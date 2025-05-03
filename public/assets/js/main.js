25f48cb549d3f989ea8586818c358b0852c4fba5
Dieguidev <diegogaraycullas@gmail.com>
2025-05-03 17:12:41 -0500

    Implementa la funcionalidad de envío de mensajes en el chatbot y actualiza el botón de traducción a "Enviar".

---
+++ public/assets/js/main.js
@@ -0,0 +1,37 @@
+const sendButton = document.querySelector("#sendButton");
+
+sendButton.addEventListener("click", async () => {
+  const inputText = document.querySelector("#inputText");
+  const myMessage = inputText.value.trim();
+
+  if (!myMessage) {
+    return false;
+  }
+
+  const messagesContainer = document.querySelector(".chat__messages");
+
+  messagesContainer.innerHTML += `<div class="chat__message chat__message--user">Yo: ${myMessage}</div>`;
+
+  inputText.value = "";
+
+  try {
+    const response = await fetch("/api/chatbot", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: myMessage }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    messagesContainer.innerHTML += `<div class="chat__message chat__message--bot">Diego: ${data.reply}</div>`;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+});
