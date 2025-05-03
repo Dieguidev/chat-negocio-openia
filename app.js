395bf4fbb511f99d4a76d90da62a5833e3a5faa3
Dieguidev <diegogaraycullas@gmail.com>
2025-05-03 14:07:34 -0500

    Elimina la funcionalidad de traducción y actualiza el diseño de la interfaz de usuario para el chat del supermercado.

--- app.js
+++ app.js
@@ -18,40 +18,6 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post("/api/traducir", async (req, res) => {
-  const { text, targetLang } = req.body;
-
-  const prompSystem1 = "Eres un traductor profesional.";
-  const prompSystem2 =
-    "Solo puedes responder con una traduccion directa del texto que el usuario te envie." +
-    "Cualquier otra respuesta o conversacion esta prohibida";
-  const prompUser = `Traduce el siguiente texto al ${targetLang}: ${text.trim()}`;
-
-  try {
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: prompSystem1 },
-        { role: "system", content: prompSystem2 },
-        { role: "user", content: prompUser },
-      ],
-      max_tokens: 500,
-      response_format: { type: "text" },
-    });
-
-    const translatedText = completion.choices[0].message.content;
-
-    return res.status(200).json({
-      translatedText,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      error: "Error al traducir el texto",
-    });
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
